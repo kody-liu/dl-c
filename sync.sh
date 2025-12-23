658405651d12ebdd65124126f221e078d997a982
Kody Liu <kodyliujiahao@gmail.com>
2025-12-23 22:57:20 +0800

    Add script

--- sync.sh
+++ sync.sh
@@ -1,1 +1,2 @@
-aws s3 sync ./local_dir s3://lie-cheater --region ap-northeast-1 --delete
+# Sync from S3 to local (use --dryrun to preview; --delete removes local files not present in S3)
+AWS_PROFILE=my-profile aws s3 sync s3://lie-cheater ./local_dir --region ap-northeast-1 --delete
